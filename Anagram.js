f9c6b5c604a78bdf331c8faf46e93b1da74a3ce4
john katua <johnkatua99@gmail.com>
2021-04-21 16:31:22 +0300

    added a functionality to compare two strings if they are an anagram

---
+++ Anagram.js
@@ -0,0 +1,14 @@
+// An anagram is another word or phrase formed by rearranging letters of the first word or phrase.
+
+const anagrams = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let sort1 = str1.split('').sort();
+  let sort2 = str2.split('').sort();
+  return sort1.join('') === sort2.join('');
+};
+
+console.log(anagrams('ann', 'nan'));
+
+// expected answer = true
